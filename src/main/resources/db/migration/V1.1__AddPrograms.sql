f7a7c6e08914ec5432a169173c829046929d089a
iuresti <iuresti@skytouchtechnology.com>
2019-08-09 23:30:50 -0500

    #60 - Create separated credentials for iOSApp, AndroidApp and ManagementApp

--- src/main/resources/db/migration/V1.1__AddPrograms.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE programs
-(
-  id          UUID PRIMARY KEY,
-  name        varchar(200) UNIQUE  NOT NULL,
-  description varchar(500)  NOT NULL,
-  responsible varchar(2000) NOT NULL
-);
