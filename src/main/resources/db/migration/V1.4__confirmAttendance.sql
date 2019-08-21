3bdb90fbbc3c7f387973448e51654894b5060564
karenmtz3 <karenmtz273@gmail.com>
2019-08-21 07:54:43 -0500

    #19 - changes of script

--- src/main/resources/db/migration/V1.4__confirmAttendance.sql
+++ src/main/resources/db/migration/V1.4__confirmAttendance.sql
@@ -4,6 +4,3 @@ CREATE TABLE confirm_attendance
     user_id     UUID REFERENCES users,
     PRIMARY KEY (session_id, user_id)
 );
-
-INSERT INTO confirm_attendance (session_id, user_id)
- VALUES ('51a8d9f3-8eb0-468d-9190-e783016ddbe8','c0926e7b-26fd-4789-a9ee-686109ef5c71');
