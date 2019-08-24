684c52ca4bf2dbd2f4d9de0ca43bdc6433363392
iuresti <iuresti@skytouchtechnology.com>
2019-08-24 12:35:17 -0500

    Fix CORS

--- src/main/java/org/desarrolladorslp/technovation/config/auth/SimpleCorsFilter.java
+++ src/main/java/org/desarrolladorslp/technovation/config/auth/SimpleCorsFilter.java
@@ -28,7 +28,7 @@ public class SimpleCorsFilter implements Filter {
         response.setHeader("Access-Control-Allow-Origin", "*");
         response.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE");
         response.setHeader("Access-Control-Max-Age", "3600");
-        response.setHeader("Access-Control-Allow-Headers", "X-Requested-With, Authorization, Content-Type, Content-Length");
+        response.setHeader("Access-Control-Allow-Headers", "*");
 
         if (HttpMethod.OPTIONS.name().equalsIgnoreCase(request.getMethod())) {
             response.setStatus(HttpServletResponse.SC_OK);
