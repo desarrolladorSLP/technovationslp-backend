c7803b8b236977d6a90cb064e0606c6fd6066ce8
karenmtz3 <karenmtz273@gmail.com>
2019-09-20 11:25:21 -0500

    #100 - Expection to delete batch

---
+++ src/main/java/org/desarrolladorslp/technovation/exception/BatchCannotBeDelete.java
@@ -0,0 +1,7 @@
+package org.desarrolladorslp.technovation.exception;
+
+public class BatchCannotBeDelete extends RuntimeException {
+    public BatchCannotBeDelete(String message){
+        super(message);
+    }
+}
