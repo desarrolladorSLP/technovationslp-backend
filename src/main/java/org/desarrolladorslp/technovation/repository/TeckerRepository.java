827a41145e53384cee8fb944e4d5a2b6d57c6625
karenmtz3 <karenmtz273@gmail.com>
2019-10-28 11:32:03 -0600

    issue #14 - change

--- src/main/java/org/desarrolladorslp/technovation/repository/TeckerRepository.java
+++ src/main/java/org/desarrolladorslp/technovation/repository/TeckerRepository.java
@@ -13,7 +13,7 @@ import java.util.UUID;
 @Repository
 public interface TeckerRepository extends JpaRepository<TeckerAssignment, UUID> {
 
-    @Query("SELECT new org.desarrolladorslp.technovation.models.DeliverableByTecker(ta.id, d.title, d.dueDate, ta.status) FROM TeckerAssigned ta INNER JOIN Deliverable d ON ta.deliverableId = d.id WHERE ta.teckerId = :teckerId")
+    @Query("SELECT new org.desarrolladorslp.technovation.models.DeliverableByTecker(ta.id, d.title, d.dueDate, ta.status) FROM TeckerAssignment ta INNER JOIN Deliverable d ON ta.deliverableId = d.id WHERE ta.teckerId = :teckerId")
     List<DeliverableByTecker> deliverableByTecker(UUID teckerId);
 
     @Query(value = "SELECT DISTINCT true FROM teckers_by_parents WHERE tecker_id = :teckerId AND parent_id = :parentId", nativeQuery = true)
